24b9a67eba4ad1af7f2159e6bfe7a47f7dabd118
OraculoTrainingen <dev@oraculo.nl>
2019-12-20 18:09:26 +0100

    first commit

---
+++ src/Notepad.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Notepad {
+    private JPanel jpanel;
+    private JTextArea textArea;
+    private JTextField textField;
+    private JButton openButton;
+
+    public Notepad() {
+        openButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JFileChooser fileChooser = new JFileChooser();
+                int status = fileChooser.showOpenDialog(jpanel);
+                if (status == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        FileReader fileReader = new FileReader(fileChooser.getSelectedFile());
+                        BufferedReader bufferedReader = new BufferedReader(fileReader);
+                        String line = bufferedReader.readLine();
+                        while (line != null)
+                        {
+                            textArea.append(line);
+                            line= bufferedReader.readLine();
+                        }
+
+                    } catch ( IOException e) {
+                        System.out.println(e.getClass());
+                    }
+                }
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Notepad");
+        frame.setContentPane(new Notepad().jpanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
